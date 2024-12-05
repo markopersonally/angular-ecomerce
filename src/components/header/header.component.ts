11c2be3b24efb1e05dc98c44797ec631869b4673
mkpage1337 <mrku1337@gmail.com>
2024-12-05 19:09:23 +0100

    commit

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -3,16 +3,8 @@ import { Component, signal } from '@angular/core';
 @Component({
   selector: 'app-header',
   imports: [],
-  template: `<div class="header">{{ title() }}</div>`,
-  styles: `
-  
-    .header {
-      background-color: gray;
-      padding: 1rem;
-      color: black;
-    }
-
-  `,
+  template: `<div class="bg-slate-100 px-4 py-3 shadow-md">{{ title() }}</div>`,
+  styles: ``,
 })
 export class HeaderComponent {
   title = signal('Angular Ecomerce');
