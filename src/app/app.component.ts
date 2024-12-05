71362a60fbbfdbd5ec806ed35aea7444549cccce
mkpage1337 <mrku1337@gmail.com>
2024-12-05 18:20:32 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: ` <router-outlet /> `,
+  imports: [
+    // RouterOutlet,
+    HeaderComponent],
+  template: `
+    <app-header />
+    <!-- <router-outlet /> -->
+  `,
   styles: [],
 })
 export class AppComponent {}
---
+++ src/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `<p>tjrieoirtj</p>`,
+  styles: ``,
+})
+export class HeaderComponent {}
