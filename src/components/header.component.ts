0f8f2487cf5bda95fb465ec3b922e0da1db0626b
mkpage1337 <mrku1337@gmail.com>
2024-12-07 21:12:15 +0100

    commit

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -6,12 +6,14 @@ import { PrimaryButtonComponent } from './primary-button.component';
   imports: [PrimaryButtonComponent],
   template: `<div class="bg-slate-100 px-4 py-3 shadow-md">
     <span>{{ title() }}</span>
-    <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+    <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"/>
   </div>`,
   styles: ``,
 })
 export class HeaderComponent {
   title = signal('Angular Ecomerce');
+
+  cart = signal("Cart");
   showButtonClicked(){
     console.log("clicked")
   }
