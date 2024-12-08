4d05701d89e3b533aa512f327ec38f25aac7e3c1
mkpage1337 <mrku1337@gmail.com>
2024-12-08 18:50:54 +0100

    commit

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -4,8 +4,8 @@ import { PrimaryButtonComponent } from './primary-button.component';
 @Component({
   selector: 'app-header',
   imports: [PrimaryButtonComponent],
-  template: `<div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
-    <span>{{ title() }}</span>
+  template: `<div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center font-bold">
+    <span class="text-xl">{{ title() }}</span>
     <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"/>
   </div>`,
   styles: ``,
--- src/components/primary-button.component.ts
+++ src/components/primary-button.component.ts
@@ -4,7 +4,7 @@ import { Component, input, output } from '@angular/core';
   selector: 'app-primary-button',
   imports: [],
   template: `<button
-    class="bg-violet-600 text-white w-full border rounded px-5 py-2"
+    class="bg-violet-600 text-white w-full border rounded-xl shadow-md px-5 py-2 hover:bg-violet-400 transition-colors"
     (click)="btnClicked.emit()"
   >
     {{ label() }}
