a47370c89645fccd41496ed1adf9953401969e9d
mkpage1337 <mrku1337@gmail.com>
2024-12-10 17:44:58 +0100

    commit

--- src/pages/products-card.component.ts
+++ src/pages/products-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../models/products.model';
+import { PrimaryButtonComponent } from '../components/primary-button.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
     <div
       class="bh-white shadow-lg border rounded-xl p-10 flex flex-col justify-center items-center relative"
@@ -12,7 +13,8 @@ import { Product } from '../models/products.model';
       <h3>{{ product().title }}</h3>
       <h3>$ {{ product().price }}</h3>
       <h3>{{ product().stock }}</h3>
-      <button>Buy</button>
+      <app-primary-button label="Add to cart" class="w-full" />
+      <app-primary-button label="Buy now!" class="w-full" />
     </div>
   `,
   styles: ``,
