dfbbcb277f5100ebc1c2073578f55b4102d9df5c
mkpage1337 <mrku1337@gmail.com>
2024-12-11 16:16:52 +0100

    commit

--- src/pages/products-card.component.ts
+++ src/pages/products-card.component.ts
@@ -11,10 +11,17 @@ import { PrimaryButtonComponent } from '../components/primary-button.component';
     >
       <img [src]="product().image" class="w-[200px] h-[100px]" />
       <h3>{{ product().title }}</h3>
-      <h3>$ {{ product().price }}</h3>
+      <h3>{{ '$' + product().price }}</h3>
       <h3>{{ product().stock }}</h3>
       <app-primary-button label="Add to cart" class="w-full" />
       <app-primary-button label="Buy now!" class="w-full" />
+      <span
+        class="absolute down-2 right-3 text-sm font-bold"
+        [class]="product().stock ? 'text-green-500' : 'text-red-500'"
+      >
+        @if (product().stock){
+        {{ product().stock }} left } @else { Out of stock }
+      </span>
     </div>
   `,
   styles: ``,
