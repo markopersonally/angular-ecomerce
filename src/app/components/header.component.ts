c750f9787c25dc9aebc79754e7511e334cb9e9ee
mkpage1337 <mrku1337@gmail.com>
2024-12-12 20:37:16 +0100

    commit

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,20 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from './primary-button.component';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-header',
   imports: [PrimaryButtonComponent],
-  template: `<div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center font-bold">
+  template: `<div
+    class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center font-bold"
+  >
     <span class="text-xl">{{ title() }}</span>
-    <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"/>
+    <app-primary-button [label]="'Cart [' + cartService.cart().length + ']'" />
   </div>`,
   styles: ``,
 })
 export class HeaderComponent {
   title = signal('Angular Ecomerce');
 
-  cart = signal("Cart");
-  showButtonClicked(){
-    console.log("clicked")
-  }
+  cartService = inject(CartService);
 }
