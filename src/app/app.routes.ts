cf7159f829fe14806ed8ecb7c28ecd101ac3608d
mkpage1337 <mrku1337@gmail.com>
2024-12-13 20:28:56 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsListComponent } from './pages/products-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ProductsListComponent,
+  },
+];
