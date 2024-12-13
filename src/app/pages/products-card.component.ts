1a4728db5daa7ded76c00af637acefbd4eb36a68
mkpage1337 <mrku1337@gmail.com>
2024-12-13 20:20:46 +0100

    commit

--- src/app/pages/products-card.component.ts
+++ src/app/pages/products-card.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '../../models/products.model';
 import { PrimaryButtonComponent } from '../components/primary-button.component';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -13,7 +14,11 @@ import { PrimaryButtonComponent } from '../components/primary-button.component';
       <h3>{{ product().title }}</h3>
       <h3>{{ '$' + product().price }}</h3>
       <h3>{{ product().stock }}</h3>
-      <app-primary-button label="Add to cart" class="w-full" />
+      <app-primary-button
+        label="Add to cart"
+        class="w-full"
+        (btnClicked)="cartService.addToCart(product())"
+      />
       <app-primary-button label="Buy now!" class="w-full" />
       <span
         class="absolute down-2 right-3 text-sm font-bold"
@@ -27,5 +32,7 @@ import { PrimaryButtonComponent } from '../components/primary-button.component';
   styles: ``,
 })
 export class ProductCardComponent {
+  cartService = inject(CartService);
+
   product = input.required<Product>();
 }
